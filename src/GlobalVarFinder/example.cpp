e7c4fe9812feac1bdf8a858a27f968d1fdb35a09
jh05013 <33805449+jh05013@users.noreply.github.com>
2019-11-28 00:35:23 +0900

    [JaeminChoi] Global variable detector

---
+++ src/GlobalVarFinder/example.cpp
@@ -0,0 +1,26 @@
+double gg = 1234;
+int arr[100];
+bool foo(int c)
+{
+    int a = c;
+    int b = gg;
+    int V = a;
+    return true;
+}
+
+void bar()
+{
+    int x = 1;
+    for (int i = 0; i < 10; ++i)
+        int y = x;
+}
+
+int a = 1, b = 2;
+int main()
+{
+    bar();
+    int a = 1;
+    int c = arr[0];
+    if (foo(1))
+        bar();
+}
