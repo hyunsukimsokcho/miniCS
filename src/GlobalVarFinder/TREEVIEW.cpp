c43e4ab932a3d5ada6c6922b668f42138ee164d4
jh05013 <jh05013@naver.com>
2019-11-29 00:30:23 +0900

    [Jaemin Choi] Scope matcher

--- src/GlobalVarFinder/TREEVIEW.cpp
+++ src/GlobalVarFinder/TREEVIEW.cpp
@@ -26,19 +26,37 @@ static StatementMatcher global_match =
     )
   ).bind("globalRef");
 
+// scope matcher
+static StatementMatcher scope_match =
+  compoundStmt(
+    hasAncestor(
+      functionDecl().bind("function")
+    )
+  ).bind("scope");
+
+/* ------------------------------------------------ */
+
 // get the location of the source range
-std::string source_location(SourceRange r, SourceManager const *sm)
+std::string source_location(SourceRange r,
+    SourceManager const *sm, bool use_end)
 {
   if(!sm) return "UNDEFINED";
   SourceLocation loc = r.getBegin();
+  SourceLocation loc_end = r.getEnd();
   PresumedLoc ploc = sm->getPresumedLoc(sm->getSpellingLoc(loc));
   std::string line = std::to_string(ploc.getLine());
   std::string col = std::to_string(ploc.getColumn());
+  if(use_end){
+    PresumedLoc ploc_end = sm->getPresumedLoc(sm->getSpellingLoc(loc_end));
+    std::string line_end = std::to_string(ploc_end.getLine());
+    std::string col_end = std::to_string(ploc_end.getColumn());
+    return line + ":" + col + " --- " + line_end + ":" + col_end;
+  }
   return line + ":" + col;
 } 
 
 // find the global variable references and print them
-struct Printer: public clang::ast_matchers::MatchFinder::MatchCallback{
+struct GlobalVarPrinter: public clang::ast_matchers::MatchFinder::MatchCallback{
   virtual void run(MatchFinder::MatchResult const &result) override{
     SourceManager &manager(
       const_cast<clang::SourceManager &>(result.Context->getSourceManager()));
@@ -47,20 +65,40 @@ struct Printer: public clang::ast_matchers::MatchFinder::MatchCallback{
     VarDecl const *gvar = result.Nodes.getNodeAs<VarDecl>("globalVar");
     if(fd && gref && gvar){
       std::cout << "Function " << fd->getNameAsString();
-      std::cout << " refers to global " << gvar->getNameAsString();
-      std::cout << " at " << source_location(gref->getSourceRange(), &manager);
+      std::cout << " refers to global " << gvar->getNameAsString() << " at ";
+      std::cout << source_location(gref->getSourceRange(), &manager, false);
       std::cout << std::endl;
     }
   }
 };
+
+// find the scopes and print them
+struct ScopePrinter: public clang::ast_matchers::MatchFinder::MatchCallback{
+  virtual void run(MatchFinder::MatchResult const &result) override{
+    SourceManager &manager(
+      const_cast<clang::SourceManager &>(result.Context->getSourceManager()));
+    FunctionDecl const *fd = result.Nodes.getNodeAs<FunctionDecl>("function");
+    CompoundStmt const *cs = result.Nodes.getNodeAs<CompoundStmt>("scope");
+    if(fd && cs){
+      std::cout << "Function " << fd->getNameAsString();
+      std::cout << " contains a scope ";
+      std::cout << source_location(cs->getSourceRange(), &manager, true);
+      std::cout << std::endl;
+    }
+  }
+};
+
+/* ------------------------------------------------ */
 
 int main(int argc, const char *argv[]){
   CommonOptionsParser optionsParser(argc, argv, toolCategory);
   ClangTool tool(optionsParser.getCompilations(),
                  optionsParser.getSourcePathList());
 
-  Printer printer;
+  GlobalVarPrinter gvprinter;
+  ScopePrinter sprinter;
   MatchFinder MF;
-  MF.addMatcher(global_match, &printer);
+  MF.addMatcher(global_match, &gvprinter);
+  MF.addMatcher(scope_match, &sprinter);
   tool.run(newFrontendActionFactory(&MF).get());
 }
