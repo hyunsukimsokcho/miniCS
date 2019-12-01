e6ab9ed28144ce6affc69dd8bc9b61c75dbe8b42
jh05013 <jh05013@naver.com>
2019-12-01 13:54:38 +0900

    [Jaemin Choi] Population candidate generator

---
+++ src/GlobalVarFinder/example_easytest.c
@@ -0,0 +1,49 @@
+#include <stdio.h>  
+#include <unistd.h>  
+
+int cnt;
+pthread_mutex_t mutex_lock;
+
+void * thread1(void *n)
+{
+    int i;
+    for (i=0; i<5; i++)
+    {
+        printf("thread1: %d\n", cnt);
+        cnt ++;
+        //sleep(1);
+    }
+}
+
+void * thread2(void *n)
+{
+    int i;
+    for(i=0; i<5; i++)
+    {
+
+        printf("thread2: %d\n", cnt);
+        cnt ++;
+        //sleep(1);
+    }
+}
+
+int main()
+{
+    pthread_mutex_init(&mutex_lock, NULL);
+    int threadId;
+    pthread_t pthread[2];
+    int status;
+    int a=1;
+
+    cnt =0;
+    pthread_create(&pthread[0], NULL, thread1, (void *)&a);
+    pthread_create(&pthread[1], NULL, thread2, (void *)&a);
+
+    pthread_join(pthread[0], (void *)&status);
+    pthread_join(pthread[1], (void *)&status);
+
+
+}
+// pthread_mutex_lock(&mutex_lock);
+// pthread_mutex_unlock(&mutex_lock);
+// pthread_mutex_destory(pthread_mutex_t *mutex); 
