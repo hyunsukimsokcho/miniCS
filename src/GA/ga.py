a713a56a99b724fed62a3308454e77011993a16a
planetarynebula <platinant@kaist.ac.kr>
2019-12-13 00:40:50 +0900

    [jaeminYu] GA algorithm with tournament selection

--- src/GA/ga.py
+++ src/GA/ga.py
@@ -5,12 +5,9 @@ import random
 import numpy as np
 import itertools
 from copy import deepcopy
-import re
 from datetime import datetime
-
-
-LOCK_START = "pthread_mutex_lock(&my_lock);\n"
-LOCK_END = "pthread_mutex_unlock(&my_lock);\n"
+import re
+import gen_pool
 
 def read_script(path):
 	lock_candidates = []
@@ -23,108 +20,7 @@ def read_script(path):
 				lock_candidates.append(tuple(map(lambda x: int(x), line.split())))
 	return lock_candidates
 
-def get_fitness_calculator(dist, cycle=True):
-	# Unimplemented!
-	return 0
 
-def get_twopoint_gauss(length, sigma=10, rand_prob=0.1):
-	start = random.randrange(0, length)
-	end = start
-	while end == start:
-		if sigma and random.random() > rand_prob:
-			end = int(random.gauss(start, sigma)) % length
-		else:
-			end = random.randrange(0, length) % length
-	return start, end
-
-def get_enclosing_sequence(ind, index, step=(-1, 1)):
-	return [ ind[i%len(ind)] for i in range(index+step[0], index+step[1]+1) ]
-
-def mutate_reverse(ind, dist_calculator, sigma=10):
-	start, end = 0, -1
-	while (start - 1)%len(ind) == end:
-		start, end = get_twopoint_gauss(len(ind), sigma)
-	start_l = (start - 1)%len(ind)
-	end_r = (end + 1)%len(ind)
-	original_distance = dist_calculator([ind[start_l], ind[start]]) + dist_calculator([ind[end], ind[end_r]])
-	distance = dist_calculator([ind[start_l], ind[end]]) + dist_calculator([ind[start], ind[end_r]])
-	if distance < original_distance:
-		if start < end:
-			sequence = ind[start:end+1]
-		else:
-			sequence = ind[start:] + ind[:end+1]
-		sequence = list(reversed(sequence))
-		if start < end:
-			mutated = ind[:start] + sequence + ind[end+1:]
-		else:
-			mutated = ind[end+1:start] + sequence
-		assert len(mutated) == len(ind)
-		return mutated, original_distance - distance
-	return None, 0
-
-def mutate_unroll(ind, dist_calculator, delta=[4,5,6,7,8,9]):
-	 start = random.randrange(len(ind))
-	 end = (start + random.choice(delta))%len(ind)
-	 if start < end:
-		 sequence = ind[start+1:end]
-	 else:
-		 sequence = ind[start+1:] + ind[:end]
-	 original_distance = dist_calculator([ind[start]] + sequence + [ind[end]])
-	 min_distance = original_distance
-	 shortest_sequence = None
-	 for perm in itertools.permutations(sequence):
-		 distance = dist_calculator([ind[start]] + list(perm) + [ind[end]])
-		 if distance < min_distance:
-			 min_distance = distance
-			 shortest_sequence = list(perm)
-	 if shortest_sequence:
-		 if start < end:
-			 mutated = ind[:start+1] + shortest_sequence + ind[end:]
-		 else:
-			 mutated = ind[end:start+1] + shortest_sequence
-		 return mutated, original_distance - min_distance
-	 return None, 0
-
-def mutate_swap(ind, dist_calculator, sigma=10):
-	start, end = get_twopoint_gauss(len(ind), sigma)
-	original_distance = dist_calculator(get_enclosing_sequence(ind, start)) + dist_calculator(get_enclosing_sequence(ind, end))
-	mutated = ind[:]
-	mutated[start], mutated[end] = ind[end], ind[start]
-	distance = dist_calculator(get_enclosing_sequence(mutated, start)) + dist_calculator(get_enclosing_sequence(mutated, end))
-	if distance < original_distance:
-		return mutated, original_distance - distance
-	return None, 0
-
-def random_gen(arr):
-	prob = 0.35
-
-	arr_len = len(arr)
-	prob_arr = np.random.rand(arr_len)
-	random.shuffle(arr)
-
-	ret = []
-
-	for i in range(arr_len):
-		if prob_arr[i] < prob:
-			is_ok = True
-			for j in ret:
-				if not ((j[1] <= arr[i][0]) or (arr[i][1] <= j[0])):
-					is_ok = False
-					break
-			if is_ok:
-				ret.append(arr[i])
-	ret.sort()
-	return ret
-
-def convert_to_multi_threaded_code(src, gene):
-	new_src = src.readlines()
-	for i, tup in enumerate(gene):
-		begin, end = tup
-		global LOCK_END
-		global LOCK_START
-		new_src.insert(2*i+begin, LOCK_START)
-		new_src.insert(2*i+end+1, LOCK_END)
-	return ''.join(new_src)
 
 if __name__ == "__main__":
 	parser = argparse.ArgumentParser()
@@ -136,14 +32,14 @@ if __name__ == "__main__":
 
 	lock_candidates = read_script(args.script)
 	offset = datetime.now().isoformat()
-	os.mkdir(offset)
-	lock_gene = random_gen(lock_candidates)
-	src = open(args.src, 'r')
-	print('lock_gene', lock_gene)
-	new_src = convert_to_multi_threaded_code(src, lock_gene)
-	with open(os.path.join(offset, '1'+'.txt'), 'w') as f:
-		f.write(new_src)
 	# Generate src files under ${datetime.now()} directory
+	os.mkdir(offset)
+
+	pool = gen_pool.GenPool(16, 100, args.src, offset, lock_candidates)
+	lock_gene = pool.get_best()
+	
+	print(lock_gene)
+
 	
 
 
---
+++ src/GA/gen_pool.py
@@ -0,0 +1,139 @@
+import random
+import numpy as np
+import evaluate
+
+def intersect_chk(arr, x):
+	for y in arr:
+		if is_intersect(x, y):
+			return True
+	return False 
+
+def is_intersect(x, y):
+	return not ((x[1] <= y[0]) or (y[1] <= x[0]))
+
+def merge_range(x, y):
+	return (min(x[0],y[0]), max(x[1],y[1]))
+
+def random_gen(arr):
+	prob = 0.35
+
+	arr_len = len(arr)
+	random.shuffle(arr)
+
+	ret = []
+
+	for i in arr:
+		if random.random() < prob:
+			if not intersect_chk(ret, i):
+				ret.append(i)
+
+	ret.sort()
+	return ret
+
+class GenPool:
+	def __init__(self, population, iteration, src, offset, arr):
+		if population % 2 > 0:
+			population = population + 1
+		self.population = population
+		self.iteration = iteration
+		self.src = src
+		self.offset = offset
+		self.arr = arr
+		self.pool = [random_gen(arr) for i in range(self.population)]
+		self.best = []
+
+	def get_best(self):
+		for i in range(self.iteration):
+			self.selection()
+		return self.best
+
+	def tournament_selection(self):
+		K = self.population // 4
+
+		ret = self.pool[0]
+		best = 0
+		for i in range(K):
+			candidate = random.choice(self.pool)
+			score = evaluate.get_score(self.src, self.offset, candidate)
+			if best < score:
+				best = score
+				ret = candidate
+		return ret
+
+	def selection(self):
+		# TODO
+
+		new_pool = []
+		for i in range(self.population // 2):
+			p0 = self.tournament_selection()
+			p1 = self.tournament_selection()
+			
+			(c0, c1) = self.crossover(p0, p1)
+			new_pool.append(c0)
+			new_pool.append(c1)
+		self.pool = new_pool
+
+		for i in self.pool:
+			if evaluate.get_score(self.src, self.offset, self.best) < evaluate.get_score(self.src, self.offset, i):
+				self.best = i
+
+		for i in range(self.population):
+			mut_prob = random.random()
+			if mut_prob < 0.1:
+				self.add_mutation(i)
+			elif mut_prob < 0.2:
+				self.remove_mutation(i)
+
+
+	def add_mutation(self, num):
+		gen = self.pool[num]
+		random.shuffle(self.arr)
+
+		for i in self.arr:
+			if not intersect_chk(gen, i):
+				gen.append(i)
+				break
+		gen.sort()
+
+	def remove_mutation(self, num):
+		gen = self.pool[num]
+		l = len(gen)
+		if l != 0:
+			del gen[np.random.randint(l)]
+
+	def crossover(self, gen0, gen1):
+		len0 = len(gen0)
+		len1 = len(gen1)
+
+		if len0 == 0 or len1 == 0:
+			return (gen0, gen1)
+
+		if len0 <= 1 and len1 <= 1:
+			return (gen0, gen1)
+
+		if len0 <=1:
+			return self.crossover(gen1, gen1)
+
+		i0 = np.random.randint(1,len0)
+		x = np.random.randint(gen0[i0-1][1], gen0[i0][0]+1)
+
+		i1 = len1
+		for j in range(len1):
+			if gen1[j][0]>=x:
+				i1 = j
+				break
+		
+		ret0 = gen0[:i0]
+		ret1 = gen1[:i1]
+
+		if i1 < len1 and is_intersect(gen0[i0-1], gen1[i1]):
+			gen0[i0-1] = merge_range(gen0[i0-1], gen1[i1])
+			del gen1[i1]
+		ret0.extend(gen1[i1:])
+
+		if i1 > 0 and is_intersect(gen1[i1-1], gen0[i0]):
+			gen1[i1-1] = merge_range(gen1[i1-1], gen0[i0])
+			del gen0[i0]
+		ret1.extend(gen0[i0:])
+
+		return (ret0, ret1)
