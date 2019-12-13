479e27c76434ac04d64b365a5879e86f7c3a9d05
ehdkacjswo <ehdkacjswo@gmail.com>
2019-12-14 05:17:42 +0900

    [Evaluate] Add machine instruction count

--- src/Evaluate/script.py
+++ src/Evaluate/script.py
@@ -3,30 +3,44 @@ import sys
 
 # lock을 걸고 푸는 위치
 # lock_list를 가지는 파일을 추가하여 거기서 얻어올 예정
-lock_list = [5, 6, 10, 11]
+lock_list = [21, 25]
 gdb.execute('set confirm off')
+gdb.execute('set pagination off')
+
+find_str = 'at 1.cpp:'
+count_num = 0
 
 with open("output.txt", "w") as f:
-    response = gdb.execute('break main', to_string=True)
+    #response = gdb.execute('break main', to_string=True)
 
     # Insert break point for every locks
     break_stmt = "break {}"
     for line in lock_list:
-        response = gdb.execute(break_stmt.format(line), to_string=True)
+        gdb.execute(break_stmt.format(line))
 
     gdb.execute('run')
-    response = gdb.execute('frame',to_string=True)
 
-    # Start recording instructions
-    gdb.execute('record full')
-    
-    # 매번 break가 걸릴때마다 response를 어떻게 처리해서 mi 개수를 가져올까
-    gdb.execute('s')
-    response = gdb.execute('frame', to_string=True)
-    f.write(response)
+    for line in lock_list:
+        while True:
+            count_num += 1
+            gdb.execute('stepi')
+            response = gdb.execute('frame', to_string = True)
+            pos = response.find(find_str)
 
-    gdb.execute('c')
-    response = gdb.execute('frame', to_string=True)
-    f.write(response)
+            if pos == -1:
+                continue
+            f.write(response)
+            response = response[pos + len(find_str):]
+            pos = response.find('\n')
+
+            response = response[:pos]
+            #f.write(response + ' ' + str(line) + '\n')
+            #if int(response) != line:
+            #   continue
+            #else:
+            #    f.write(response + ' ' + str(count_num))
+            #    break
+
+            gdb.execute('continue')
 
 gdb.execute('quit')
